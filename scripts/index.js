33c0ccb807ff48bc3ebd606b2d8f3a6374108c61
ratan <ratanchakma68@gmail.com>
2024-02-14 11:04:47 +0600

    game function add

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,19 @@
-function play(){
+
+function continueGame() {
+    const alphabet = getRandomNumber();
+    const displayAlphabet = document.getElementById("display");
+    displayAlphabet.innerText = alphabet.toUpperCase();
+    addBackgroundId(alphabet);
+
+}
+
+
+
+
+function play() {
     removeElement("home");
     addElement("play-ground")
-
+    continueGame()
 }
 
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -9,3 +9,21 @@ function addElement(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove("hidden");
 }
+
+function getRandomNumber(){
+    const alphabetString = "abcdefgijklmnopqrstuvwxyz";
+    const alphabetArray = alphabetString.split("");
+    // console.log(alphabetArray);
+
+    // get random number 0-25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    // console.log(alphabetArray[index]);
+  return alphabetArray[index];
+
+}
+
+function addBackgroundId (elementID){
+    const element =  document.getElementById(elementID);
+    element.classList.add("bg-red-500");
+}
