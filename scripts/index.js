52c1b36ad61319db37ceb50088bbfe21cf3a59ea
ratan <ratanchakma68@gmail.com>
2024-02-15 11:42:25 +0600

    game app done

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,41 @@
 
+function handelKeyboardButtonPress(e) {
+    const playerPressed = e.key;
+    // console.log(playerPressed);
+    // stop the game pres home
+    if (playerPressed === '1') {
+        gameOver()
+    }
+    // get the expected get element
+    const currentAlphabetElement = document.getElementById("display")
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedOutput = currentAlphabet.toLocaleLowerCase();
+
+    if (playerPressed === expectedOutput) {
+        const scoreElement = getDisplayElement("display-score");
+        const updatedScore = scoreElement + 1;
+        setTextElementById("display-score", updatedScore);
+        removeBackgroundId(expectedOutput)
+        continueGame()
+
+    } else {
+        const lifeElement = getDisplayElement("life-element");
+        const updatedLife = lifeElement - 1;
+        setTextElementById("life-element", updatedLife)
+
+        if (updatedLife === 0) {
+            gameOver();
+
+        }
+    }
+
+}
+
+
+document.addEventListener("keyup", handelKeyboardButtonPress)
+
+
+
 function continueGame() {
     const alphabet = getRandomNumber();
     const displayAlphabet = document.getElementById("display");
@@ -7,13 +44,28 @@ function continueGame() {
 
 }
 
-
-
-
 function play() {
+    // hide every element show only play-ground
     removeElement("home");
     addElement("play-ground")
+    removeElement("scroll-element");
+    // reset 
+    setTextElementById("life-element", 5)
+    setTextElementById("display-score", 0)
     continueGame()
 }
 
+function gameOver() {
+    removeElement("play-ground");
+    addElement("scroll-element")
+
+    // get last score
+    const lastScore = getDisplayElement("display-score");
+    setTextElementById("final-score", lastScore);
+
+    // clear the last selected alphabet
+    const currentElement = getElementTextById("display");
+    removeBackgroundId(currentElement.toLocaleLowerCase());
+
+}
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,20 +10,49 @@ function addElement(elementId) {
     element.classList.remove("hidden");
 }
 
-function getRandomNumber(){
+function getRandomNumber() {
     const alphabetString = "abcdefgijklmnopqrstuvwxyz";
     const alphabetArray = alphabetString.split("");
     // console.log(alphabetArray);
 
     // get random number 0-25
-    const randomNumber = Math.random()*25;
+    const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
     // console.log(alphabetArray[index]);
-  return alphabetArray[index];
+    return alphabetArray[index];
 
 }
 
-function addBackgroundId (elementID){
-    const element =  document.getElementById(elementID);
+function addBackgroundId(elementID) {
+    const element = document.getElementById(elementID);
     element.classList.add("bg-red-500");
+}
+function removeBackgroundId(elementID) {
+    const element = document.getElementById(elementID);
+    element.classList.remove("bg-red-500");
+}
+
+
+/* score function */
+// display-score
+function getDisplayElement(elementId) {
+    const scoreOrLifeElement = document.getElementById(elementId);
+    const elementValue = scoreOrLifeElement.innerText;
+    const displayValue = parseInt(elementValue);
+
+    return displayValue;
+}
+
+/*  */
+function setTextElementById(element, value) {
+    const currentElement = document.getElementById(element);
+    currentElement.innerText = value;
+}
+
+/*  */
+function getElementTextById(elementID) {
+    const elementText = document.getElementById(elementID);
+    const text = elementText.innerText;
+    return text;
+
 }
